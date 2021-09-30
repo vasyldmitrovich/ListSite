7d41b720e106648fef67ea4f367862968d981e53
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2021-09-30 16:33:01 +0300

    Add DateFormat to convert array objects to Json

--- src/main/Java/org/list_of_deals/modal/service/ConvertToJson.java
+++ src/main/Java/org/list_of_deals/modal/service/ConvertToJson.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.list_of_deals.modal.entity.CourtCase;
 
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ConvertToJson {
 
     //This method create json from array Court Cases
-    public String arrCourtCaseToJson (ArrayList<CourtCase> arrayList) {
+    public String arrCourtCaseToJson (ArrayList<CourtCase> arrayList, String dateFormatStr) {
         StringBuilder jsonString = new StringBuilder();
-
         try {
             ObjectMapper objectMapper = new ObjectMapper();
+            DateFormat dateFormat = new SimpleDateFormat(dateFormatStr);
+            objectMapper.setDateFormat(dateFormat);
                 jsonString.append(objectMapper.writerWithDefaultPrettyPrinter()
                 .writeValueAsString(arrayList));
         } catch (JsonProcessingException e) {
